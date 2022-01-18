6b72048379ac71e2b92d24a20c7f7e018d298ed4
Rifkialbot2 <93379131+Rifkialbot2@users.noreply.github.com>
2022-01-18 07:41:09 +0700

    Update _help.py

--- plugins/_help.py
+++ plugins/_help.py
@@ -45,19 +45,19 @@ async def _help(ult):
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Official"][plug]:
                     output += i
-                output += "\n© @TeamUltroid"
+                output += "\n© @kingswibu"
                 await ult.eor(output)
             elif HELP.get("Addons") and plug in HELP["Addons"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Addons"][plug]:
                     output += i
-                output += "\n© @TeamUltroid"
+                output += "\n© @kingswibu"
                 await ult.eor(output)
             elif HELP.get("VCBot") and plug in HELP["VCBot"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["VCBot"][plug]:
                     output += i
-                output += "\n© @TeamUltroid"
+                output += "\n© @kingswibu"
                 await ult.eor(output)
             else:
                 try:
@@ -65,7 +65,7 @@ async def _help(ult):
                     for d in LIST[plug]:
                         x += HNDLR + d
                         x += "\n"
-                    x += "\n© @TeamUltroid"
+                    x += "\n© @kingswibu"
                     await ult.eor(x)
                 except BaseException:
                     await ult.eor(get_string("help_1").format(plug), time=5)
